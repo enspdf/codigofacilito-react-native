c540def34bce80753abc3f7c63aacaa5d51c242d
Sebastian Higuita <sebastianhc1995@hotmail.com>
2019-03-25 17:34:33 -0500

    Add a input search to query contacts to add an event

--- components/contacts/ContactCard.js
+++ components/contacts/ContactCard.js
@@ -6,9 +6,12 @@ import styles from "../../stylesheets/base.stylesheet";
 export default props => {
   return (
     <Card style={styles.card}>
-      <Card.Content>
-        <Image source={{ uri: props.user.avatar }} />
-        <View>
+      <Card.Content style={{ flexDirection: "row" }}>
+        <Image
+          style={{ width: 40, height: 40 }}
+          source={{ uri: props.user.avatar }}
+        />
+        <View style={{ flex: 1, marginLeft: 10 }}>
           <Title>{props.user.name}</Title>
         </View>
         <IconButton
--- components/contacts/ContactsUI.js
+++ components/contacts/ContactsUI.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import { View } from "react-native";
 import styles from "../../stylesheets/base.stylesheet";
 import { FlatList } from "react-native-gesture-handler";
 import Empty from "../utilities/Empty";
 import ContactCard from "./ContactCard";
+import { TextInput } from "react-native-paper";
 
-export default props => {
-  return (
-    <View style={styles.container}>
-      <FlatList
-        ListEmptyComponent={Empty}
-        data={props.contacts}
-        renderItem={({ item }) => (
-          <ContactCard
-            addContactToEvent={props.addContactToEvent}
-            user={item}
-          />
-        )}
-      />
-    </View>
-  );
-};
+export default class ContactsUI extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: ""
+    };
+  }
+
+  updateQuery = text => {
+    this.setState({ query: text });
+    this.props.queryContacts(text);
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          label="Buscar un contacto..."
+          value={this.state.query}
+          onChangeText={this.updateQuery}
+        />
+        <FlatList
+          ListEmptyComponent={Empty}
+          data={this.props.contacts}
+          renderItem={({ item }) => (
+            <ContactCard
+              addContactToEvent={this.props.addContactToEvent}
+              user={item}
+            />
+          )}
+        />
+      </View>
+    );
+  }
+}
